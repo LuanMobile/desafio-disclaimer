cfebb6909778019bcf910b841f51aa9cf1872898
LuanMobile <luanhenriique.ls13@gmail.com>
2023-07-19 20:00:48 -0300

    Implementado lançamentos por files

--- app/Http/Controllers/LancamentosController.php
+++ app/Http/Controllers/LancamentosController.php
@@ -20,16 +20,32 @@ class LancamentosController extends Controller
         return response()->json($data, 201);
     }
     
-    public function store() {
+    public function sendFile(Request $req, $id) {
+
+        $client = Clientes::find($id);
+
+        if ($req->hasFile('file')) {
+            $file = $req->file('file');
+            $fileContent = file_get_contents($file->getRealPath());
     
+            // Criar um novo lançamento para o cliente e salvar o conteúdo do arquivo nele
+            $lancamento = new Lancamentos();
+            $lancamento->file_lancamentos = $fileContent;
+            //dd($lancamento);
+            $client->lancamentos()->save($lancamento);
+    
+            return response()->json(['message' => 'Arquivo enviado e salvo com sucesso!']);
+        } else {
+            return response()->json(['message' => 'Nenhum arquivo foi enviado.'], 400);
+        }
+    }
+
+    public function store() {
+        
         $lancamentos = Lancamentos::select('lancamentos')->orderBy('created_at', 'desc')->get();
         //dd($lancamentos);
         return response()->json(
-         $lancamentos,
+          $lancamentos,
             200);
     }
 }
-
-
-/* TABELA Lançamentos */                                                /* valores-lacamentos */
-/* id | client_id | lanc1 | lanc2 | created_at | updated_at */                          /* id | lanc_id | descricao | salario | valor */
